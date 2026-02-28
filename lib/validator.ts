7c3425a130730df9d60bf2a978c61cbaa5697ab6
yunjimin <ge72946@gmail.com>
2026-02-28 14:04:46 +0900

    custom sign in

--- lib/validator.ts
+++ lib/validator.ts
@@ -46,29 +46,29 @@ export const ProductInputSchema = z.object({
 });
 
 export const OrderItemSchema = z.object({
-  clientId: z.string().min(1, 'clientId is required'),
-  product: z.string().min(1, 'Product is required'),
-  name: z.string().min(1, 'Name is required'),
-  slug: z.string().min(1, 'Slug is required'),
-  category: z.string().min(1, 'Category is required'),
+  clientId: z.string().min(1, "clientId is required"),
+  product: z.string().min(1, "Product is required"),
+  name: z.string().min(1, "Name is required"),
+  slug: z.string().min(1, "Slug is required"),
+  category: z.string().min(1, "Category is required"),
   quantity: z
     .number()
     .int()
-    .nonnegative('Quantity must be a non-negative number'),
+    .nonnegative("Quantity must be a non-negative number"),
   countInStock: z
     .number()
     .int()
-    .nonnegative('Quantity must be a non-negative number'),
-  image: z.string().min(1, 'Image is required'),
-  price: Price('Price'),
+    .nonnegative("Quantity must be a non-negative number"),
+  image: z.string().min(1, "Image is required"),
+  price: Price("Price"),
   size: z.string().optional(),
   color: z.string().optional(),
-})
+});
 
 export const CartSchema = z.object({
   items: z
     .array(OrderItemSchema)
-    .min(1, 'Order must contain at least one item'),
+    .min(1, "Order must contain at least one item"),
   itemsPrice: z.number(),
   taxPrice: z.optional(z.number()),
   shippingPrice: z.optional(z.number()),
@@ -76,4 +76,37 @@ export const CartSchema = z.object({
   paymentMethod: z.optional(z.string()),
   deliveryDateIndex: z.optional(z.number()),
   expectedDeliveryDate: z.optional(z.date()),
-})
+});
+
+// USER
+const UserName = z
+  .string()
+  .min(2, { message: "Username must be at least 2 characters" })
+  .max(50, { message: "Username must be at most 30 characters" });
+const Email = z.string().min(1, "Email is required").email("Email is invalid");
+const Password = z.string().min(3, "Password must be at least 3 characters");
+const UserRole = z.string().min(1, "role is required");
+
+export const UserInputSchema = z.object({
+  name: UserName,
+  email: Email,
+  image: z.string().optional(),
+  emailVerified: z.boolean(),
+  role: UserRole,
+  password: Password,
+  paymentMethod: z.string().min(1, "Payment method is required"),
+  address: z.object({
+    fullName: z.string().min(1, "Full name is required"),
+    street: z.string().min(1, "Street is required"),
+    city: z.string().min(1, "City is required"),
+    province: z.string().min(1, "Province is required"),
+    postalCode: z.string().min(1, "Postal code is required"),
+    country: z.string().min(1, "Country is required"),
+    phone: z.string().min(1, "Phone number is required"),
+  }),
+});
+
+export const UserSignInSchema = z.object({
+  email: Email,
+  password: Password,
+});
