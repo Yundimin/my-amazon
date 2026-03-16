842902f283f6123c6715412190d4e4ec00f8b480
yunjimin <ge72946@gmail.com>
2026-03-16 20:55:28 +0900

    search page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,29 +1,28 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import qs from 'query-string'
+import qs from "query-string";
 
 export function formUrlQuery({
   params,
   key,
   value,
 }: {
-  params: string
-  key: string
-  value: string | null
+  params: string;
+  key: string;
+  value: string | null;
 }) {
-  const currentUrl = qs.parse(params)
+  const currentUrl = qs.parse(params);
 
-  currentUrl[key] = value
+  currentUrl[key] = value;
 
   return qs.stringifyUrl(
     {
       url: window.location.pathname,
       query: currentUrl,
     },
-    { skipNull: true }
-  )
+    { skipNull: true },
+  );
 }
-
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -159,3 +158,38 @@ export const formatDateTime = (dateString: Date) => {
 export function formatId(id: string) {
   return `..${id.substring(id.length - 6)}`;
 }
+
+export const getFilterUrl = ({
+  params,
+  category,
+  tag,
+  sort,
+  price,
+  rating,
+  page,
+}: {
+  params: {
+    q?: string;
+    category?: string;
+    tag?: string;
+    price?: string;
+    rating?: string;
+    sort?: string;
+    page?: string;
+  };
+  tag?: string;
+  category?: string;
+  sort?: string;
+  price?: string;
+  rating?: string;
+  page?: string;
+}) => {
+  const newParams = { ...params };
+  if (category) newParams.category = category;
+  if (tag) newParams.tag = toSlug(tag);
+  if (price) newParams.price = price;
+  if (rating) newParams.rating = rating;
+  if (page) newParams.page = page;
+  if (sort) newParams.sort = sort;
+  return `/search?${new URLSearchParams(newParams).toString()}`;
+};
